f4b43b3c89139f7894d56245f4699aad041df6e6
DeMassou <98885191+DeMassou@users.noreply.github.com>
2022-02-03 09:15:03 +0100

    CHORE: Added Basic HTML/CSS and added Routing

---
+++ src/app/Login-Trainer/login-trainer.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//Component (decorator) Created for login-trainer
+@Component ({ 
+    selector: 'app-login-trainer',
+    templateUrl: './login-trainer.page.html',
+    styleUrls: ['./login-trainer.page.css']
+})
+
+export class LoginTrainerPage {
+
+}
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,31 @@
+import { Component, NgModule } from "@angular/core";
+import { Router, RouterModule, Routes } from "@angular/router";
+import { LoginTrainerPage } from "./Login-Trainer/login-trainer.page";
+
+//1. Landing Page
+//2. Catalogue Page
+//3. Trainer Page
+
+
+//Route Created to Navigate
+const routes: Routes = [
+
+    //if its a empty path redirect to login page as default
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/login'
+    },
+    {
+        path: 'login',
+        component: LoginTrainerPage,
+
+    }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}

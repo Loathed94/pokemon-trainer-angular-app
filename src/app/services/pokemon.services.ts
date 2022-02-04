9eb690b21f864781457a8ee4f40ae7aaf12cfa9b
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-02-04 08:46:34 +0100

    chore: Added a Map to increase search efficiency when fetching a pokemon using name.
    Also added an update method that keeps sessionStorage (and in the future API) updated.

--- src/app/services/pokemon.services.ts
+++ src/app/services/pokemon.services.ts
@@ -10,6 +10,7 @@ export class PokemonService{
     private pokemonWithImg: PokemonWithImage[] = [];
     private error: string = '';
     private imgURL: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+    private pokeNameMap: Map<string, PokemonWithImage> = new Map();
 
     constructor(private readonly http: HttpClient){
 
@@ -19,6 +20,9 @@ export class PokemonService{
         if(sessionStorage.getItem('pokemon') !== null){
             const pokeStorage: PokemonWithImage[] = JSON.parse(sessionStorage.getItem('pokemon') || '');
             this.pokemonWithImg = pokeStorage;
+            for(let i = 0; i < this.pokemonWithImg.length; i++){
+                this.pokeNameMap.set(this.pokemonWithImg[i].pokemon.name, this.pokemonWithImg[i]);
+            }
             return;
         }
         this.http.get<PokemonRawData>('https://pokeapi.co/api/v2/pokemon?limit=200')
@@ -26,7 +30,9 @@ export class PokemonService{
             this.pokemon = pokemonRaw.results;
             for(let i = 0; i < this.pokemon.length; i++){
                 const pokeImgURL: string[] = this.pokemon[i].url.split('/');
-                this.pokemonWithImg.push({pokemon: this.pokemon[i], img: `${this.imgURL}${pokeImgURL[pokeImgURL.length-2]}.png`, id: parseInt(pokeImgURL[pokeImgURL.length-2]), collected: false});
+                const newPokemon: PokemonWithImage = {pokemon: this.pokemon[i], img: `${this.imgURL}${pokeImgURL[pokeImgURL.length-2]}.png`, id: parseInt(pokeImgURL[pokeImgURL.length-2]), collected: false};
+                this.pokemonWithImg.push();
+                this.pokeNameMap.set(newPokemon.pokemon.name, newPokemon);
             }
             sessionStorage.setItem('pokemon', JSON.stringify(this.pokemonWithImg));
         }, (error: HttpErrorResponse) =>{
@@ -44,5 +50,17 @@ export class PokemonService{
 
     public collectPokemon(pokemon: PokemonWithImage): void{
         pokemon.collected = true;
+        this.updateStorageAndTrainer();
+    }
+
+    private updateStorageAndTrainer(): void{
+        sessionStorage.setItem('pokemon', JSON.stringify(this.pokemonWithImg));
+        //Update trainer locally and on API
+    }
+
+    public collectPokemonWithName(name: string): void {
+        const namedPokemon: PokemonWithImage = this.pokeNameMap.get(name) || {pokemon: {name: '', url: ''}, img: '', id: NaN, collected: false};
+        namedPokemon.collected = true;
+        this.updateStorageAndTrainer();
     }
 }

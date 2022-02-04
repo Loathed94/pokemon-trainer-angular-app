c63b06ae914683b17165eb283ce74a95e701405e
DeMassou <98885191+DeMassou@users.noreply.github.com>
2022-02-04 09:51:57 +0100

    FEAT: Trainer now adds to storage and redirects to catalogue page

--- src/app/catalogue/catalogue.page.ts
+++ src/app/catalogue/catalogue.page.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
+import { TrainersService } from "../services/trainer.service";
 
 @Component({
     selector: 'app-catalogue',
     templateUrl: './catalogue.page.html',
     styleUrls: ['./catalogue.page.css']
 })
-export class CataloguePage{}
+export class CataloguePage{
+
+    get trainername(): string {
+        return this.trainerService.trainername
+    }
+
+    constructor(
+        private trainerService: TrainersService
+    ) {}
+}
+

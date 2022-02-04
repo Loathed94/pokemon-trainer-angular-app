0b5776f8319a77ffc2aa3e0634b9f4b5ef50a1a6
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-02-04 13:30:38 +0100

    chore: Added API-Key to environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  pokemonAPI: "https://lechan-noroff-api.herokuapp.com/trainers"
+  pokemonAPI: "https://lechan-noroff-api.herokuapp.com/trainers",
+  apiKey: 'n1ZKh0vT2oxeCvrWBefmxhBLDpFw0bouiHRtG1OjzIgol1Lm2TWdWSIcD4kj8Zic'
 };

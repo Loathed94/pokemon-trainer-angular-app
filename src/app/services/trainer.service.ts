3cf93ba2f555ce247178cfa1fa5f7f81aaa42c7b
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-02-07 11:36:44 +0100

    docs: Comments added to methods in trainerService.

--- src/app/services/trainer.service.ts
+++ src/app/services/trainer.service.ts
@@ -14,45 +14,41 @@ const URL = environment.pokemonAPI;
     providedIn: 'root'
 })
 export class TrainersService{
-    //private _trainers: string = "";
     private _trainer: Trainer | null = null;
-    private error: string = '';
+    //private error: string = '';
 
-    /*ngOnInit(): void {
-        if(this._trainer === null && localStorage.getItem(TRAINER_KEY) !== null){
-            console.log("OnInit Trainer")
-            this._trainer = JSON.parse(localStorage.getItem(TRAINER_KEY) || '');
-        }
-    }*/
-
+    //If a page is refreshed while there is a trainer in storage the service will need to refresh its own variable of the trainer.
     public updateTrainerFromStorage(): void{
         if(localStorage.getItem(TRAINER_KEY) !== null){
-            console.log("OnInit Trainer")
             this._trainer = JSON.parse(localStorage.getItem(TRAINER_KEY) || '');
         }
     }
 
+    //Updates or sets the trainer in localStorage, mainly called when a trainer is updated, for example getting a new pokemon.
     public updateTrainerInStorage(): void{
         localStorage.setItem(TRAINER_KEY, JSON.stringify(this._trainer));
     }
 
+    //A getter for the trainer's name. 
     get trainerName(): string {
         return this._trainer?.username || '';
     }
 
+    //A getter for the trainer-object.
     get trainer(): Trainer | null{
         return this._trainer;
     }
 
+    //A setter for the service's trainer object.
     public setTrainer(trainer: Trainer): void{
         this._trainer = trainer;
     }
 
-    //Session Storage dosent remove the user after refresh
     constructor(private readonly http: HttpClient, private router: Router) { 
         
     }
 
+    //Creates a header needed for POST/PATCH api-calls.
     private createHeaders(): HttpHeaders {
         return new HttpHeaders({
             'Content-Type': 'application/json',
@@ -60,7 +56,7 @@ export class TrainersService{
         })
     }
 
-    ///Add/Create new user to API
+    //Fetching trainer using the name user input in login page if the trainer exists. Otherwise a trainer is created and posted on the API.
     public addTrainer(username: string): void {
         this.http.get<Trainer[]>(`${URL}?username=${username}`)
         .subscribe((trainer: Trainer[]) => {
@@ -83,29 +79,18 @@ export class TrainersService{
                 this.router.navigateByUrl("/catalogue");
             }
         })
-        /*const user = {
-            username: username,
-            pokemon: []
-        }
-        const headers = this.createHeaders()
-        
-        return this.http.post(URL + "", user, {
-            headers
-
-        })*/
     }
 
-    
+    //When a pokemon is collected this method is called, it adds a pokemon name to the list of pokemon names that the trainer possesses.
     public addPokemonToTrainer(pokemon: PokemonWithImage):void{
         this._trainer?.pokemon.push(pokemon.pokemon.name);
     }
 
-
+    //A HTTP-patch call that updates the trainer on the API with its list of pokemon.
     public patchTrainerPokemon(): void{
         const headers = this.createHeaders();
         this.http.patch<Trainer>(`${URL}/${this._trainer?.id}`, {pokemon: this._trainer?.pokemon}, {headers})
         .subscribe((response: Trainer) => {
-            //this._trainer = response;
             localStorage.setItem(TRAINER_KEY, JSON.stringify(response));
         });
     }

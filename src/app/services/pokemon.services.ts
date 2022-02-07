18a434974908006c8b009c0bcc509393aac44389
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-02-07 11:51:11 +0100

    docs: Added comments to methods in pokemonServices.

--- src/app/services/pokemon.services.ts
+++ src/app/services/pokemon.services.ts
@@ -19,6 +19,9 @@ export class PokemonService{
 
     }
 
+    //The method first checks if there is a list of pokemon in sessionStorage, if so it fetches that information to the state and returns.
+    //Otherwise it performs a fetch from API and stores the results in this state and in sessionStorage. 
+    //Both versions of the method will also iterate over trainer's pokemon and mark those as collected in the list of pokemon stored here.
     public fetchPokemon(trainer: Trainer | null): void{
         if(sessionStorage.getItem('pokemon') !== null){
             const pokeStorage: PokemonWithImage[] = JSON.parse(sessionStorage.getItem(this.POKEMON_KEY) || '');
@@ -49,14 +52,18 @@ export class PokemonService{
         })
     }
 
+    //A getter that returns all pokemon stored in state.
     public getPokemon(): PokemonWithImage[]{
         return this.pokemonWithImg;
     }
 
+    //A getter that returns error (not really used, delete?).
     public getError(): string{
         return this.error;
     }
 
+    //A method that resets all pokemon stored in sessionStorage making sure all have collected = false. 
+    //Used when login-page is loaded without logged in trainer, new trainer should not automatically receive pokemon from another one.
     public resetPokemonList(): void{
         const pokemonList: PokemonWithImage[] = JSON.parse(sessionStorage.getItem(this.POKEMON_KEY) || '');
         for(let i = 0; i < pokemonList.length; i++){
@@ -66,32 +73,24 @@ export class PokemonService{
         sessionStorage.setItem(this.POKEMON_KEY, JSON.stringify(this.pokemonWithImg));
     }
 
+    //Changes a specific pokemon's collected property to true and adds its name to the trainer objects list of pokemon.
     public collectPokemon(pokemon: PokemonWithImage, trainer: Trainer | null): void{
         pokemon.collected = true;
         trainer!.pokemon.push(pokemon.pokemon.name);
         this.updateStorageAndTrainer();
     }
 
+    //Method updates the list of pokemon stored in sessionStorage, called upon when a pokemon's properties are changed. 
     private updateStorageAndTrainer(): void{
         sessionStorage.setItem('pokemon', JSON.stringify(this.pokemonWithImg));
         //Update trainer locally and on API
     }
 
+    //Uses a pokemon's name as string to mark it as collected using a hashmap that stores the same pokemon objects as the array.
+    //Allows for easier access to specific pokemon. Used when a trainer is fetched that already owns some pokemon.
     public collectPokemonWithName(name: string): void {
         const namedPokemon: PokemonWithImage = this.pokeNameMap.get(name) || {pokemon: {name: '', url: ''}, img: '', id: NaN, collected: false};
         namedPokemon.collected = true;
         this.updateStorageAndTrainer();
     }
-
-    /*private createHeaders(){
-        return new HttpHeaders({
-            'Content-Type': 'application/json',
-            'x-api-key': 'API-KEY'
-        })
-    }
-
-    patchFunction(payload: string): void{
-        const headers = this.createHeaders()
-        this.http.patch('api url', payload, {headers})
-    }*/
 }
